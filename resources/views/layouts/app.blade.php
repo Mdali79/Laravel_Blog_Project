f281775fc51044d8b8ba2bdd8e3707d90cd23372
incognito2 <mohamodali2446@gmail.com>
2023-03-13 21:53:05 +0600

    further changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,7 @@
 
         <title>{{ config('app.name') }}</title>
 
-        <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
-        
-
+      
         <style>
             body {
                 font-family: 'Nunito';
